df128f1ff32f7f3e361cf48def8f4d5431dd7706
neu_net <przman2000@mail.ru>
2022-01-25 00:34:02 +1000

    semantic skeleton update to v.2

--- source/semantic analyzer/SemanticAnalyzer.py
+++ source/semantic analyzer/SemanticAnalyzer.py
@@ -31,9 +31,10 @@ class VariableSemanticAnalyser:
         # TODO: Исходя из ноды <тип данных> -> bool|char|short найти и вернуть тип данных
         return 0
 
-    def addVariableHelper(self, node: Node):
+    def addVariable(self, node: Node):  # Input: value = <инициализация переменной> || <объявление переменной>
         newVariable = Variable(None, None)
         typeCheck = None
+        errorCheck = None
         for part in node.children:
             if part.value.name == '<выражение>':
                 typeCheck = self.findExpressionType(part)
@@ -44,12 +45,50 @@ class VariableSemanticAnalyser:
         if typeCheck and typeCheck != newVariable.type_v:
             # TODO: добовить в типы ошибок enum с семантическими ошибками. Добавить ошибку несоответствия типов
             print("Error")
-        else:
+            errorCheck = True
+        reservedWords = []  # TODO: найти/объявить все зарезервированные слова
+        if newVariable.name in reservedWords:
+            # TODO: добовить в типы ошибок enum с семантическими ошибками. Добавить ошибку использование
+            #  зарезервированного слова
+            print("Error")
+            errorCheck = True
+        for variable in self.variables:
+            if variable.name == newVariable.name:
+                # TODO: добовить в типы ошибок enum с семантическими ошибками. Добавить ошибку многократного
+                #  объявления переменной в области видимости
+                print("Error")
+                errorCheck = True
+        if errorCheck is None:
             # TODO: хз почему variables - это не list
             self.variables.append(newVariable)
 
-    def parse(self):
-        print(self.tree)
+    def updateVariableCheck(self, node: Node):  # Input: value = <обновление переменной>
+        typeCheck = None
+        nameCheck = None
+        exist = None
+        for part in node.children:
+            if part.value.name == '<выражение>':
+                typeCheck = self.findExpressionType(part)
+            if part.value.name == '<имя переменной>':
+                nameCheck = self.findName(part)
+        for variable in self.variables:
+            if variable.name == nameCheck:
+                exist = True
+                if variable.type_v != typeCheck:
+                    # TODO: добовить в типы ошибок enum с семантическими ошибками. Добавить ошибку несоответствия типов
+                    print('Error')
+        if exist is None:
+            # TODO: добовить в типы ошибок enum с семантическими ошибками. Добавить ошибку не существования переменной
+            print('Error')
+
+    def parse(self, node):
+        if node.value.name == '<инициализация переменной>' or node.value.name == '<объявление переменной>':
+            self.addVariable(node)
+        if node.value.name == '<обновление переменной>':
+            self.updateVariableCheck(node)
+        if not node.children:
+            for nextNode in node.children:
+                self.parse(nextNode)
 
 
 class FunctionSemanticAnalyser:
